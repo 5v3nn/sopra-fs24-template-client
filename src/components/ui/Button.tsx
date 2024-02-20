d7ec6651e3e75f513d2d45069f63c9638a7122cf
5v3nn <sven.greuter@protonmail.ch>
2024-02-20 19:41:10 +0100

    run prettier

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../../styles/ui/Button.scss";
 
-export const Button = props => (
-  <button
-    {...props}
-    style={{width: props.width, ...props.style}}
-    className={`primary-button ${props.className}`}>
-    {props.children}
-  </button>
+export const Button = (props) => (
+    <button
+        {...props}
+        style={{ width: props.width, ...props.style }}
+        className={`primary-button ${props.className}`}
+    >
+        {props.children}
+    </button>
 );
 
-
 Button.propTypes = {
-  width: PropTypes.number,
-  style: PropTypes.string,
-  className: PropTypes.string,
-  children: PropTypes.node,
+    width: PropTypes.number,
+    style: PropTypes.string,
+    className: PropTypes.string,
+    children: PropTypes.node,
 };
