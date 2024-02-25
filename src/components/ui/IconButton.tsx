dc22707d71e953f09df59bb1c205da262209b600
5v3nn <sven.greuter@protonmail.ch>
2024-02-25 11:26:09 +0100

    created component IconButton
    - with css

---
+++ src/components/ui/IconButton.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "../../styles/ui/Button.scss";
+import "../../styles/ui/IconButton.scss";
+
+const IconButton = (props) => {
+  const { hoverIcon: HoverIcon, icon: Icon, ...rest } = props;
+  const [hover, setHover] = useState(false);
+
+  return (
+    <button
+      {...rest}
+      style={{
+        width: props.width,
+        background: "none",
+        border: "none",
+        padding: 0,
+        color: "inherit",
+        cursor: "pointer",
+        ...props.style,
+      }}
+      className={`primary-button icon-button ${props.className}`}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {hover && HoverIcon ? (
+        <HoverIcon size={20} className={"icon-button-icon"} />
+      ) : (
+        <Icon size={20} className={"icon-button-icon"} />
+      )}
+      {props.children}
+    </button>
+  );
+};
+
+IconButton.propTypes = {
+  width: PropTypes.number,
+  style: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
+  icon: PropTypes.func,
+  hoverIcon: PropTypes.func,
+};
+
+export default IconButton;
